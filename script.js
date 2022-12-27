44cf9bdfff713c1a11273032c5f5620f424afe66
Luiz <lhvalentim@outlook.coom>
2022-12-27 08:47:22 -0300

    fix

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 navigator.serviceWorker?.register('service-worker.js')
 
+refreshPageSize = () => {
+	let isStandalone = window.matchMedia('(display-mode: standalone)').matches
+	document.documentElement.style.setProperty('--vh', `${isStandalone ? window.outerHeight : window.innerHeight}px`)
+}
+
 document.onreadystatechange = () => {
 	if (document.readyState != 'complete') return
+	refreshPageSize()
 	let contaienr = document.querySelector('header section')
 	contaienr.onmousewheel = e => {
 		contaienr.scrollBy({left: (e.wheelDelta * -1)})
 	}
+}
+
+window.onresize = () => {
+	refreshPageSize()
 }
