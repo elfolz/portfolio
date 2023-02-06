1f078b4b39f7a35e8c04b9d3d1bc0f8ea724460c
Luiz <lhvalentim@outlook.coom>
2023-02-06 10:43:54 -0300

    add hero

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-navigator.serviceWorker?.register('service-worker.js').then(reg => {
-	reg.addEventListener('updatefound', () => {
-		let newWorker = reg.installing
-		newWorker?.addEventListener('statechange', () => {
-			console.log('Update Installed. Restarting...')
-			if (newWorker.state == 'activated') location.reload(true)
-		})
-	})
-})
+navigator.serviceWorker?.register('service-worker.js')
+navigator.serviceWorker.onmessage = m => {
+	console.info('Update found!')
+	if (m?.data == 'update') location.reload(true)
+}
 
 refreshPageSize = () => {
 	let isStandalone = window.matchMedia('(display-mode: standalone)').matches
