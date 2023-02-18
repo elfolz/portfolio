0cd41cafaafb6ab2e2805e8fa33acbb8e952ad72
Luiz <lhvalentim@outlook.coom>
2023-02-18 11:30:36 -0300

    add astro

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,10 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('fetch', event => {
-	event.respondWith(caches.open('webgl').then(cache => {
+	event.respondWith(caches.open('Elfolz').then(cache => {
 		return cache.match(event.request)
 		.then(cachedResponse => {
-			let cachedFile = cachedResponse.clone().blob()
+			const cachedFile = cachedResponse.clone().blob()
 			const fetchedResponse = fetch(event.request)
 			.then(networkResponse => {
 				if (networkResponse.status == 200) cache.put(event.request, networkResponse.clone())
@@ -15,7 +15,7 @@ self.addEventListener('fetch', event => {
 					networkResponse.clone().blob()
 				]).then(response => {
 					if (!event.clientId) return
-					if (response[0].size == response[1].size) return
+					if (response[0]?.size === response[1]?.size) return
 					self.clients.get(event.clientId)
 					.then(client => {
 						client?.postMessage('update')
